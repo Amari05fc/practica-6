a1758dd688ec0b85f00273bd6a57191952aa8422
Amari05fc <fercruher@gmail.com>
2024-09-29 21:02:59 -0600

    Actualizacion

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,8 @@ import (
 	"fmt"
 
 	"github.com/Amari05fc/practica-6/cmd/config"
-	
 )
 
 func main() {
-	fmt.Println("Main")
 	fmt.Println(config.PASSWORD)
-	fmt.Println("Variables de entorno: ", config.USERNAME, config.PASSWORD)
 }
